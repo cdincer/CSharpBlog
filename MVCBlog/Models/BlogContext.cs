643d67927c01df80a960469785e4688118fb093e
Can Dincer <dincercan@gmail.com>
2017-09-16 15:51:16 +0300

    EF- Stuff added

---
+++ MVCBlog/Models/BlogContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MVCBlog.Models
+{
+    public class BlogContext:DbContext
+    {
+
+        public DbSet<BlogPost> BlogTables { get; set; }
+    }
+}
