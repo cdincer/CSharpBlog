9e6e38fdfdcf8783e60b42b76bca4f2b4c7bba78
CanD <dincercan@gmail.com>
2017-09-20 18:52:10 +0300

    Blog Editor added. Controller rename

---
+++ Models/BlogContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MVCBlog.Models
+{
+    public class BlogContext : DbContext
+    {
+        public BlogContext() : base("name=DefaultConnection")
+        { }
+       
+
+        public DbSet<BlogPost> BlogTables { get; set; }
+    }
+}
