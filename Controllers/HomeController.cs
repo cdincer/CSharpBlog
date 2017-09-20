59984b6a133d9f2e56cb10903453700571bf3ee0
cdincer <dincercan@gmail.com>
2017-09-20 17:29:45 +0300

    Add files via upload
    UserTables added. Blog Poster and Editer pages for UI

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCBlog.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private MVCBlog.Models.BlogContext db = new MVCBlog.Models.BlogContext();
+
+        // GET: HomeS
+        public ActionResult Index()
+        {
+            return View(db.BlogTables);
+        }
+
+
+        //This is for reading a Blog Post not actually posting it.
+        public ActionResult BlogPostPage()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/BlogPost.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCBlog.Models
+{
+    public class BlogPost
+    {
+        public int ID { get; set; }
+        public string PostTitle { get; set; }
+        public string PostContent { get; set; }
+        public DateTime PostDate { get; set; }
+    }
+}
